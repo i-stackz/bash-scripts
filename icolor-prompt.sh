424a7c0c590ea07b4773b9a3878e395eb4e33c6c
i-stackz <steve.stackz@gmail.com>
2025-11-06 18:16:28 -0500

    updated the icolor-prompt.sh script

--- icolor-prompt.sh
+++ icolor-prompt.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 
+
 # Description: Script to generate a random colored prompt upon user login
+
 
 # Function to generate a random color code
 function random_color_code(){
@@ -26,7 +28,7 @@ RNUM4=$(( RANDNOM % 10 ));
 RNUM5=$(( RANDNOM % 10 ));
 
 # Define colors
-COLOR1="\[\e[5;$(random_color_code)m\]"; # will make the color prompt blink (rm '5;' to make it not blink)
+COLOR1="\[\e[5;$(random_color_code)m\]";
 COLOR2="\[\e[${RNUM2};$(random_color_code)m\]";
 COLOR3="\[\e[${RNUM3};$(random_color_code)m\]";
 COLOR4="\[\e[${RNUM4};$(random_color_code)m\]";
@@ -34,42 +36,12 @@ COLOR5="\[\e[${RNUM5};$(random_color_code)m\]";
 COLOR_RESET="\[\e[0m\]";
 
 # Set PS1 with random color codes
-PS1="${COLOR1}-->${COLOR_RESET} ${COLOR2}\u${COLOR_RESET}${COLOR1}@${COLOR_RESET}${COLOR3}\h${COLOR_RESET} ${COLOR1}[${COLOR_RESET}${COLOR4}\W${COLOR_RESET}${COLOR1}]${COLOR_RESET} ${COLOR2}\\\$${COLOR_RESET} ${COLOR1}:${COLOR_RESET} ";
+PS1="${COLOR1}[|${COLOR_RESET}\A${COLOR1}|]${COLOR_RESET}${COLOR1}-->${COLOR_RESET} ${COLOR2}\u${COLOR_RESET}${COLOR1}@${COLOR_RESET}${COLOR3}\h${COLOR_RESET} ${COLOR1}[${COLOR_RESET}${COLOR4}\W${COLOR_RESET}${COLOR1}]${COLOR_RESET} ${COLOR2}\\\$${COLOR_RESET} ${COLOR1}:${COLOR_RESET}  ";
 
 # print out PS1
 echo $PS1;
 
 
-#========================================================================================================================#
-
-# NOTE:
+# NOTES:
 # In order for the script to work you need to place the line 'export PS1=$(sh color-prompt.sh)' within your .bashrc file
 # and also have the script within the same path as the .bashrc file
-
-#========================================================================================================================#
-
-
-#############
-# my banner #
-#############
-
-##########################################################
-# source: http://patorjk.com/software/taag/		 #
-##########################################################
-echo -e "\nScript by: ";
-echo -e "\n";
-echo -e "\e[5;31m";
-echo -e " ██▓ ██████▄▄▄█████▓▄▄▄      ▄████▄  ██ ▄█▒███████▒";
-echo -e "▓██▒██    ▒▓  ██▒ ▓▒████▄   ▒██▀ ▀█  ██▄█▒▒ ▒ ▒ ▄▀░";
-echo -e "▒██░ ▓██▄  ▒ ▓██░ ▒▒██  ▀█▄ ▒▓█    ▄▓███▄░░ ▒ ▄▀▒░ ";
-echo -e "░██░ ▒   ██░ ▓██▓ ░░██▄▄▄▄██▒▓▓▄ ▄██▓██ █▄  ▄▀▒   ░";
-echo -e "░██▒██████▒▒ ▒██▒ ░ ▓█   ▓██▒ ▓███▀ ▒██▒ █▒███████▒";
-echo -e "░▓ ▒ ▒▓▒ ▒ ░ ▒ ░░   ▒▒   ▓▒█░ ░▒ ▒  ▒ ▒▒ ▓░▒▒ ▓░▒░▒";
-echo -e " ▒ ░ ░▒  ░ ░   ░     ▒   ▒▒ ░ ░  ▒  ░ ░▒ ▒░░▒ ▒ ░ ▒";
-echo -e " ▒ ░  ░  ░   ░       ░   ▒  ░       ░ ░░ ░░ ░ ░ ░ ░";
-echo -e " ░       ░               ░  ░ ░     ░  ░    ░ ░    ";
-echo -e "                            ░             ░        ";
-echo -e "\e[0m";
-echo -e "\n";
-###########################################################
-
