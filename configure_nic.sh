7269f679738fe77e7df0a5e6008b71031cda341b
i-stackz <steve.stackz@gmail.com>
2026-01-18 20:46:55 -0500

    made changes to script

--- configure_nic.sh
+++ configure_nic.sh
@@ -2,7 +2,7 @@
 
 <<-"COMMENT"
 	
-	Description: This script will set your NICs configuration from automatically being assigned an IPv4 address from DHCP to a statically assigned one
+	Description: This script will set your NICs configuration from automatically being assigned an IPv4 address from DHCP to static. For hosts with up to 2 NICs
 	OS: RHEL 9
 	Author: istackz
 	Date: 01/14/2026
@@ -18,62 +18,82 @@ fi
 # variables * you should adjust these variables to suit your needs *
 dns1="192.168.1.53"
 dns2="192.168.1.54"
-dns_domain="myhost.example.com"
+dns_domain="localhost.home"
 
+# check for number of nics on host
 number_of_nics=$(ip -brief addr | grep -iv 'lo' | wc -l)
-
 
 # logic for 1 NIC
 if (( number_of_nics == 1 ))
 then
-	nic=$(ip -brief addr | grep -iv 'lo')
+	# grab nic info
+	nic=$(ip -brief addr | grep -iv 'lo' | head -n 1)
 
 	# split the nic variable into parts
 	read -r iface state ipv4 _ <<< "$nic";
 
+	# display output
 	echo -e "\n<----------->";
 	echo "iface: $iface";
 	echo "state: $state";
 	echo "ipv4: $ipv4";
 	echo "<------------->\n";
 
+	# get NetworkManager's connection name for the nic
 	connection_name="$(nmcli -t -f name connection show | grep -iv 'lo')"
 
+	# check ipv4 variable contents
 	if [[ ! "$ipv4" =~ \b([0-9]{1,3}\.){3}[0-9]{1,3}\b ]]
 	then
+		# search file for 'search' parameter
 		if [[ -z $(grep -i '^search' /etc/resolv.conf) ]]
 		then
+			# add search parameter to file
 			echo "search $dns_domain" >> /etc/resolv.conf
 		fi
 
+		# search file for 'search' parameter setting
 		if [[ $(grep -i '^search' /etc/resolv.conf | cut -d ' ' -f 2-) =~ $dns_domain ]]
 		then
+			# search and replace
 			sed -r -i "s/search .*/search $dns_domain/1" /etc/resolv.conf
 		fi
 
+		# Logic to check file for multiple 'nameserver' entries
 		if [[ $(grep -i '^nameserver' /etc/resolv.conf | wc -l) == 1 ]]
 		then
+			# check if 'nameserver' setting matches comparison
 			if [[ $(grep -i '^nameserver' /etc/resolv.conf | awk '{print $2}') != $dns1 ]]
-			then
+			then	
+				# search and replace
 				sed -r -i "s/^namerserver .*/nameserver $dns1/1" /etc/resolv.conf
 			fi
 		elif [[ $(grep -i '^nameserver' /etc/resolv.conf | wc -l) == 2 ]]
 		then
+			# if 1st 'nameserver' entry does not match comparison
 			if [[ $(grep -i '^nameserver' /etc/resolv.conf | head -n 1 | cut -d ' ' -f 2) != $dns1 ]]
 			then
+				# search and replace
 				sed -r -i "s/^nameserver .*/nameserver $dns1/1" /etc/resolv.conf;
-			elif [[ $(grep -i '^nameserver' /etc/resolv.conf | tail -n 1 | awk '{print $2}') != $dns2 ]]
+			fi
+	
+			# if 2nd 'nameserver' entry does not match comparison
+			if [[ $(grep -i '^nameserver' /etc/resolv.conf | tail -n 1 | awk '{print $2}') != $dns2 ]]
 			then
+				# search and replace
 				sed -r -i "0,/^nameserver/! { /^nameserver .*/nameserver $dns2/ }" /etc/resolv.conf
 			fi
 		fi
 
+		# if no 'nameserver' entry found
 		if [[ -z $(grep -i '^nameserver' /etc/resolv.conf) ]]
 		then
+			# add to file
 			echo "nameserver $dns1" >> /etc/resolv.conf
 		fi
-
-		ipv4=$(nslookup $HOSTNAME | awk '/^Address: / { print $2 }' | tail -n 1)
+		
+		# use nslookup binary to get IPv4 address from dns 
+		ipv4=$(nslookup $HOSTNAME | awk '/Address: / {for(i=1;i<=NF;i++) if($i ~ /^([0-9]{1,3}\.){3}[0-9]{1,3}$/) print $i}');
 
 		if [[ ! $connection_name =~ $iface ]]
 		then
@@ -112,9 +132,10 @@ then
 	connection_name2=$(nmcli -t -f name connection show | grep -iv 'lo' | tail -n 1)
 	count=0
 
+	# like a for loop but keeps lines in tact
 	while IFS= read -r nic
 	do
-		# split lines into words
+		# split into words and store in variables
 		read -r iface state ipv4 _ <<< "$nic"
 
 		echo -e "\n<-------->"
@@ -140,83 +161,99 @@ then
 
 				if [[ -z $(grep -i '^nameserver' /etc/resolv.conf) ]]
 				then
-					echo "nameserver $dns1" >> /etc/resolv.conf
+					echo "nameserver $dns1" >> /etc/resolv.conf;
 				fi
 
 				if [[ $(grep -i '^nameserver' /etc/resolv.conf | wc -l) == 1 ]]
 				then
 					if [[ $(grep -i '^nameserver' /etc/resolv.conf | awk '{print $2}') != $dns1 ]]
 					then
-						sed -r -i "s/^nameserver .*/nameserver $dns1/1" /etc/resolv.conf
+						sed -r -i "s/^nameserver .*/nameserver $dns1/1" /etc/resolv.conf;
 					fi
 				elif [[ $(grep -i '^nameserver' /etc/resolv.conf | wc -l) == 2 ]]
 				then
 					if [[ $(grep -i '^nameserver' /etc/resolv.conf | head -n 1 | awk '{print $2}') != $dns1 ]]
 					then
-						sed -r -i "s/^nameserver .*/nameserver $dns1/1" /etc/resolv.conf
+						sed -r -i "s/^nameserver .*/nameserver $dns1/1" /etc/resolv.conf;
 					elif [[ $(grep -i '^nameserver' /etc/resolv.conf | tail -n 1 | awk '{print $2}') != $dns2 ]] 
 					then
-						sed -r -i "0,/^nameserver/! { /^nameserver/ s/^nameserver .*/nameserver $dns2" /etc/resolv.conf
+						sed -r -i "0,/^nameserver/! { /^nameserver/ s/^nameserver .*/nameserver $dns2" /etc/resolv.conf;
 					fi
 				fi
 
-				ipv4=$(nslookup $HOSTNAME | awk '/^Address: / { print $2 }' | tail -n 1)
+				# grab ipv4 of 1st nic
+				ipv4=$(nslookup $HOSTNAME | awk '/Address: / {for(i=1;i<=NF;i++) if($i ~ /^([0-9]{1,3}\.){3}[0-9]{1,3}$/) print $i}' | head -n 1);
 
+				# check if match
 				if [[ $connection_name1 != $face ]]
 				then
-					nmcli connection modify $connection_name1 connection.id $iface
+					# set connection_name1 to iface
+					nmcli connection modify $connection_name1 connection.id $iface;
 				fi
 
+				# configure 1st nic
 				nmcli connection modify $connection_name1 \
 					ipv4.method manual \
 					ipv4.addresses "$ipv4/24" \
 					ipv4.gateway "${ipv4%.*}.1" \
 					ipv4.dns "$dns1,$dns2" \
-					ipv4.dns-search "$dns_domain" \
+					ipv4.dns-search "$dns_domain";
 			else
+				# temporary variables
 				temp="${ipv4%/*}" # string manipulation to remove '/24' from variable
 				gateway="${temp%.*}.1" # string manipulation to edit the last octet of variable to '.1'
 
+				# check comparison
 				if [[ $connection_name1 != $iface ]]
 				then
-					nmcli connection modify $connection_name1 connection.id $iface
+					# set connection_name1 to iface
+					nmcli connection modify $connection_name1 connection.id $iface;
 				fi
 
+				# configure 1st nic
 				nmcli connection modify $connection_name1 \
 					ipv4.method manual \
 					ipv4.addresses $ipv4 \
 					ipv4.gateway $gateway \
 					ipv4.dns "$dns1,$dns2" \
-					ipv4.dns-search "$dns_domain"
+					ipv4.dns-search "$dns_domain";
 			fi
 		fi
 
 		# if count == 1; then we are on the 2nd nic
 		if (( count == 1 ))
 		then
+			# if ipv4 does not match regex
 			if [[ ! $ipv4 =~ ([0-9]{1,3}\.){3}[0-9]{1,3} ]]
 			then
 				if [[ -z $(nslookup "$HOSTNAME-<suffix1>") ]]
 				then
-					ipv4=$(nslookup "$HOSTNAME-<suffix2" | awk '/^Address: / { last = $NF } END { print last }')
+					ipv4=$(nslookup "$HOSTNAME-<suffix2>" | awk '/^Address: / { last = $NF } END { print last }');
 				else
-					ipv4=$(nslookup "$HOSTNAME-<suffix1" | awk '/^Address: / { last = $NF } END { print last }')
+					ipv4=$(nslookup "$HOSTNAME-<suffix1>" | awk '/^Address: / { last = $NF } END { print last }');
 				fi
-
+				
+				# configure 2nd nic
 				nmcli connection modify $connection_name2 \
 					ipv4.method manual \
 					ipv4.addresses "$ipv4/24" \ 
-					connection.id $iface
+					connection.id $iface;
 			else
+				# configure 2nd nic
 				nmcli connection modify $connection_name2 \
 					ipv4.method manual \
 					ipv4.addresses "$ipv4/24" \
-					connection.id $iface
+					connection.id $iface;
 			fi
 		fi
+
+		# increment the count variable by 1
+		(( count++ ));
 	done < <(ip -brief addr | grep -iv 'lo')
 fi
 
+# reload NetworkManager configurations
 nmcli connection reload
 
+# exit successfully
 exit 0
